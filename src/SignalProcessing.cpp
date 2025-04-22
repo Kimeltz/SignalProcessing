a410eaf6a804f2c502f0533770f57a100184276c
Kimeltz <ikmaltsani0@gmail.com>
2025-04-22 23:08:39 +0700

    Added Moving Standard Deviation

--- src/SignalProcessing.cpp
+++ src/SignalProcessing.cpp
@@ -49,6 +49,39 @@ int movingAverage::getCount()
     return _count;
 }
 
+movingStandardDeviation::movingStandardDeviation(int bufferSize)
+    :_size(bufferSize), _index(0), _count(0), _sum(0.0f), _sumSquare(0.0f), _buffer(nullptr) {}
+
+movingStandardDeviation::~movingStandardDeviation() {
+    if (_buffer) free(_buffer);
+}
+
+bool movingStandardDeviation::init()
+{
+    if (_buffer) free(_buffer);
+    _buffer = (float*) calloc(_size, sizeof(float));
+    return (_buffer != nullptr);
+}
+
+float movingStandardDeviation::update(float newData)
+{
+    if (!_buffer) return 0.0f;
+    if(_count == _size) {
+        _sum -= _buffer[_index];
+        _sumSquare -= _buffer[_index] * _buffer[_index];
+    } else {
+        _count++;
+    }
+
+    _buffer[_index] = newData;
+    _sum += newData;
+    _sumSquare += newData * newData;
+    _index = (_index + 1) % _size; 
+
+    float mean = _sum / _count;
+    return sqrt((_sumSquare / _count) - (mean * mean));
+}
+
 float roundToNearest005(float val)
 {
     return round(val * 20) / 20;
@@ -60,4 +93,19 @@ float* roundToNearest005(float* val, int size)
         val[i] = round(val[i] * 20) / 20;
     }
     return val;
+}
+
+float average(float* val, int size)
+{
+    float sum = 0.0f;
+    for(int i = 0; i < size; i++) sum += val[i];
+    return sum / size;
+}
+
+float standardDeviation(float* val, int size)
+{
+    float mean = average(val, size);
+    float sum = 0.0f;
+    for(int i = 0; i < size; i++) sum += pow(val[i] - mean, 2);
+    return sqrt(sum / size);
 }
--- src/SignalProcessing.h
+++ src/SignalProcessing.h
@@ -31,7 +31,7 @@ class movingAverage
          /**
          * @brief Menginisialisasi buffer dan memori.
          * 
-         * @return int Status inisialisasi, 0 jika berhasil, -1 jika gagal.
+         * @return bool Status inisialisasi, 0 jika berhasil, -1 jika gagal.
          */
         bool init();
 
@@ -84,7 +84,100 @@ class movingAverage
 
 };
 
+class movingStandardDeviation
+{
+    public:
+         /**
+         * @brief Konstruktor untuk class movingStandardDeviation.
+         * 
+         * Konstruktor ini akan menginisialisasi ukuran buffer, indeks, jumlah
+         * data yang diterima, dan buffer itu sendiri.
+         * 
+         * @param bufferSize Ukuran dari buffer yang digunakan untuk menyimpan data.
+         */
+        movingStandardDeviation(int bufferSize);
+
+         /**
+         * @brief Destruktor untuk class movingStandardDeviation.
+         * 
+         * Fungsi ini digunakan untuk membebaskan memori yang digunakan oleh buffer
+         * ketika objek dihancurkan.
+         */
+        ~movingStandardDeviation();
+
+         /**
+         * @brief Mengupdate nilai deviasi standar bergerak dengan data baru.
+         * 
+         * Fungsi ini menghitung deviasi standar bergerak setelah menerima data baru.
+         * Jika jumlah data sudah mencapai ukuran buffer, maka data yang lama
+         * akan digantikan dengan yang baru.
+         * 
+         * @param newData Data baru yang akan dimasukkan ke dalam buffer.
+         * @return float Nilai deviasi standar bergerak setelah data baru dimasukkan.
+         */
+        float update(float newData);
+
+        /**
+         * @brief Menginisialisasi buffer dan memori.
+         * 
+         * @return bool Status inisialisasi, 0 jika berhasil, -1 jika gagal.
+         */
+        bool init();
+
+    private:
+        int _size; 
+        float* _buffer;
+        int _index;
+        int _count;
+        float _sum;
+        float _sumSquare;
+
+};
+
+/**
+ * @brief Fungsi untuk membulatkan nilai ke kelipatan 0.05.
+ * 
+ * Fungsi ini digunakan untuk membulatkan nilai float ke kelipatan 0.05 terdekat.
+ * 
+ * @param val Nilai yang akan dibulatkan.
+ * @return float Nilai yang sudah dibulatkan.
+ */
 float roundToNearest005(float val);
+
+/**
+ * @brief Fungsi untuk membulatkan array nilai ke kelipatan 0.05.
+ * 
+ * Fungsi ini digunakan untuk membulatkan setiap elemen dalam array float ke
+ * kelipatan 0.05 terdekat.
+ * 
+ * @param val Pointer ke array nilai yang akan dibulatkan.
+ * @param size Ukuran dari array.
+ * @return float* Pointer ke array yang sudah dibulatkan.
+ */
 float* roundToNearest005(float* val, int size);
+
+/**
+ * @brief Fungsi untuk menghitung rata-rata dari array nilai.
+ * 
+ * Fungsi ini digunakan untuk menghitung rata-rata dari setiap elemen dalam
+ * array float.
+ * 
+ * @param val Pointer ke array nilai yang akan dihitung rata-ratanya.
+ * @param size Ukuran dari array.
+ * @return float Rata-rata dari nilai-nilai dalam array.
+ */
+float average(float* val, int size);
+
+/**
+ * @brief Fungsi untuk menghitung deviasi standar dari array nilai.
+ * 
+ * Fungsi ini digunakan untuk menghitung deviasi standar dari setiap elemen
+ * dalam array float.
+ * 
+ * @param val Pointer ke array nilai yang akan dihitung deviasi standarnya.
+ * @param size Ukuran dari array.
+ * @return float Deviasi standar dari nilai-nilai dalam array.
+ */
+float standardDeviation(float* val, int size);
 
 #endif
